fdb723a03001e6e1565180cd6109f1b6cc8a457b
Alisson <alisson.teles.ribeiro@gmail.com>
2021-01-20 22:52:31 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const dinito = document.querySelector('dinito');
+
+function handlekeyup(event){
+    if (event.keyCode === 32) {
+
+    }
+}
+
+function jump() {
+    let position = 0;
+    let upInterval = setInterval (() => {
+
+    }, 20);
+}
+
+document.addEventListener('keyup' , );
